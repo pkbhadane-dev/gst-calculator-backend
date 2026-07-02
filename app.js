025dba6c4dc0bc4058438392014973a70305464b
Prashant Bhadane <pkbhadane545@gmail.com>
2026-07-02 20:44:52 +0530

    feat: restructure routes and add database connection logic

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import clientRouter from "./src/routes/client.routes";
+import userRouter from "./src/routes/user.routes";
+import invoiceRouter from "./src/routes/invoice.route";
+const app = express();
 
-const app = express()
+app.use(cookieParser());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/client", clientRouter);
+app.use("/api/v1/invoice", invoiceRouter);
 
 app.get("/", (req, res) => {
-    res.send("Well-Come Home")
-})
+  res.send("Well-Come Home");
+});
 
-export default app
 
+export default app;
